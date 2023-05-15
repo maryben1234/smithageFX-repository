f80dfd771b71a3e0e039f6e5a4656378472266d7
Mary <101870828+Chioma227@users.noreply.github.com>
2023-05-15 07:51:01 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import './App.css';
+import './index.css';
+import { Router, Routes, Route } from 'react-router';
+import Contact from './pages/contact/Contact';
 import Home from './pages/landing-pages/Home';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Routes>
+      <Route exact path='/' element={ <Home />}/>
+     <Route path='/contact' element={<Contact/>}/>
+    </Routes>
   );
 }
 
