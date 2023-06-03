24b4d908ed9d982edc778e7fd89d829defdc49b3
Ellovick1 <ezekielelom1@gmail.com>
2023-06-03 21:45:40 +0100

    addded our services to the home page

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -8,6 +8,7 @@ import { Carousel } from 'react-responsive-carousel';
 // import Rotate from 'react-reveal/Rotate';
 import { AnimationOnScroll } from 'react-animation-on-scroll';
 import 'animate.css/animate.min.css';
+import { Link } from 'react-router-dom'
 
 
 export default function Carousels() {
@@ -22,10 +23,21 @@ export default function Carousels() {
 
                     </div>
                     <AnimationOnScroll animateIn="animate__fadeInUp" className='carousel-text'>
-                            <div >
-                                <p>
+                    <div className='p-4 pb-10'>
+                                <p className='text-left text-4xl mb-4'>
                                     Welcome to SmithFx
                                 </p>
+
+                                <p className=' text-left text-base mb-4'>SmithageFX Academy offers a range of training programs, from basic to professional, focused on teaching individuals how to navigate the forex market successfully.</p>
+                                
+                                <button>
+                                <Link to="/academy">
+                                <a className='bg-blue-600 shadow-md px-3 py-4 text-xs hover:bg-white hover:text-blue-600 hover:border-blue-600' href="" target="_blank" rel="noopener noreferrer">
+                                Click here to learn more
+                                </a>
+                                </Link>
+                                </button>
+                                
                             </div>
                         </AnimationOnScroll>
                 </div>
@@ -36,10 +48,19 @@ export default function Carousels() {
                     <div className='md:h-[800px] h-fit'>
                         <img src={Image2} className='' />
                         <AnimationOnScroll animateIn="animate__fadeInUp" className='carousel-text'>
-                            <div >
-                                <p>
+                        <div className='p-4 pb-10'>
+                                <p className='text-left text-4xl mb-4'>
                                     Welcome to SmithFx
                                 </p>
+
+                                <p className=' text-left text-base mb-4'>Get daily payouts with our award winning partner program.</p>
+                                
+                                <button>
+                                <a className='bg-blue-600 shadow-md px-3 py-4 text-xs hover:bg-white hover:text-blue-600 hover:border-blue-600' href="https://www.myfxbook.com" target="_blank" rel="noopener noreferrer">
+                                Click here to learn more
+                                </a>
+                                </button>
+                                
                             </div>
                         </AnimationOnScroll>
                     </div>
@@ -51,10 +72,19 @@ export default function Carousels() {
                     <div className='md:h-[800px] h-fit'>
                         <img src={Image3} className='' />
                         <AnimationOnScroll animateIn="animate__fadeInUp" className='carousel-text'>
-                            <div >
-                                <p>
+                        <div className='p-4 pb-10'>
+                                <p className='text-left text-4xl mb-4'>
                                     Welcome to SmithFx
                                 </p>
+
+                                <p className=' text-left text-base mb-4'>Manage your accounts and access insightful reports and technical analysis among many more features.</p>
+                                
+                                <button>
+                                <a className='bg-blue-600 shadow-md px-3 py-4 text-xs hover:bg-white hover:text-blue-600 hover:border-blue-600' href="https://portal.equiticlients.com" target="_blank" rel="noopener noreferrer">
+                                Click here to learn more
+                                </a>
+                                </button>
+                                
                             </div>
                         </AnimationOnScroll>
                     </div>
